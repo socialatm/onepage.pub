099503c2d76bd989e7afd113df7089b1af50d2b6
Ray <raypeaslee@gmail.com>
2024-03-15 21:23:07 -0400

    a bit closer

--- assets/js/twitta.js
+++ assets/js/twitta.js
@@ -12,24 +12,12 @@ console.log(form);
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const formData = new FormData();
-  formData.append('@context', document.getElementById('@context').value)
-  formData.append('type', 'note')
-  formData.append('attributedTo', document.getElementById('attributedTo').value)
-  formData.append('content', document.getElementById('content').value)
-  formData.append('to', document.getElementById('to').value)
-  
-  alert(formData.get('@context'))
-  alert(formData.get('type'))
-  alert(formData.get('attributedTo'))
-  alert(formData.get('content'))
-  alert(formData.get('to'))
+  const formData = new FormData(form);
+  console.dir(...formData)
 
-  axios.post('https://localhost:65380/orderedcollection/oKIVOg6PQTPq76MTPDTG-', { FormData }, {
-    headers: {
-      'Content-Type': 'multipart/form-data'
-    }
-  })
+  // above here is working
+
+  axios.post('https://localhost:65380/orderedcollection/oKIVOg6PQTPq76MTPDTG-', formData )
     .then(response => {
       // Handle the response
       console.log(JSON.stringify(response, null, 2));
