cb9d9e9544f30580cd0a679c6a14c4357af1ceae
Ray <raypeaslee@gmail.com>
2024-03-17 08:11:23 -0400

    bump rate limit
    matches what mastodon uses.
    I guess I was being way too conservative.

--- index.mjs
+++ index.mjs
@@ -2420,7 +2420,7 @@ app.use(session({
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: RATE_LIMIT, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	limit: RATE_LIMIT, // Limit each IP to 900 requests per `window` (here, per 15 minutes). Equals 1 per second.
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	// store: ... , // Use an external store for consistency across multiple server instances.
