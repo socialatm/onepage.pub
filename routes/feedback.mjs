583d73590cad2b2b1bdee63a356aeecb25dcc110
Ray <raypeaslee@gmail.com>
2024-03-27 12:04:36 -0400

    feedback uses the page module

--- routes/feedback.mjs
+++ routes/feedback.mjs
@@ -1,8 +1,22 @@
 import { Router } from 'express'
+import page from '../modules/page.mjs'
+import passport from 'passport'
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.send('Feedback page');
+router.get('/',  passport.authenticate('session'), (req, res) => {
+  if (!req.isAuthenticated()) {
+    logger.error('Not authenticated')
+    res.redirect('/login')
+  }
+
+  const user = req.user
+  if (!user) {
+    throw new createError.InternalServerError('Invalid user even though isAuthenticated() is true')
+    res.redirect('/login')
+  }
+
+  const feedbackHtml = `<div>Feedback Page</div>`
+  res.send(page('Feedback', feedbackHtml, user));
 });
 
 export default router
