dca97ea2c8ed2c2fc2a275a667338be5f4817d07
Ray <raypeaslee@gmail.com>
2024-03-27 12:14:18 -0400

    profile and settings use the page module

--- routes/actor.mjs
+++ routes/actor.mjs
@@ -1,12 +1,38 @@
 import { Router } from 'express'
+import page from '../modules/page.mjs'
+import passport from 'passport'
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.send('Actor page');
+router.get('/',  passport.authenticate('session'), (req, res) => {
+  if (!req.isAuthenticated()) {
+    logger.error('Not authenticated')
+    res.redirect('/login')
+  }
+
+  const user = req.user
+  if (!user) {
+    throw new createError.InternalServerError('Invalid user even though isAuthenticated() is true')
+    res.redirect('/login')
+  }
+
+  const actorHtml = `<div>Actor Page</div>`
+  res.send(page('Actor', actorHtml, user));
 });
 
-router.get('/settings', (req, res) => {
-    res.send('Settings page');
-  });
+router.get('/settings',  passport.authenticate('session'), (req, res) => {
+  if (!req.isAuthenticated()) {
+    logger.error('Not authenticated')
+    res.redirect('/login')
+  }
+
+  const user = req.user
+  if (!user) {
+    throw new createError.InternalServerError('Invalid user even though isAuthenticated() is true')
+    res.redirect('/login')
+  }
+
+  const settingsHtml = `<div>Settings Page</div>`
+  res.send(page('Settings', settingsHtml, user));
+});
 
 export default router
