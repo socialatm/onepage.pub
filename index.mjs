c7ca26523065f0bddef25d338c6b8b1cb5a5a0b0
Ray <raypeaslee@gmail.com>
2024-03-29 12:02:15 -0400

    get context for index.mjs

--- index.mjs
+++ index.mjs
@@ -75,7 +75,7 @@ const SEC_CONTEXT = 'https://w3id.org/security'
 const BLOCKED_CONTEXT = 'https://purl.archive.org/socialweb/blocked'
 const PENDING_CONTEXT = 'https://purl.archive.org/socialweb/pending'
 const WEBFINGER_CONTEXT = 'https://purl.archive.org/socialweb/webfinger'
-const CONTEXT = [AS_CONTEXT, SEC_CONTEXT, BLOCKED_CONTEXT, PENDING_CONTEXT, WEBFINGER_CONTEXT]
+export const CONTEXT = [AS_CONTEXT, SEC_CONTEXT, BLOCKED_CONTEXT, PENDING_CONTEXT, WEBFINGER_CONTEXT]
 
 const LD_MEDIA_TYPE = 'application/ld+json'
 const ACTIVITY_MEDIA_TYPE = 'application/activity+json'
