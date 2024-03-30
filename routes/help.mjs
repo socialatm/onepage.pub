4dd58c3d3688294d3ef27c5ed1118dad77e90645
Ray <raypeaslee@gmail.com>
2024-03-30 15:04:13 -0400

    use the help page without logging in

--- routes/help.mjs
+++ routes/help.mjs
@@ -1,23 +1,12 @@
 import { Router } from 'express'
 import page from '../modules/page.mjs'
-import passport from 'passport'
 import limiter from '../modules/rate-limit.mjs'
 
 const router = Router()
 router.use(limiter)
+const user = null
 
-router.get('/',  passport.authenticate('session'), (req, res) => {
-  if (!req.isAuthenticated()) {
-    logger.error('Not authenticated')
-    res.redirect('/login')
-  }
-
-  const user = req.user
-  if (!user) {
-    throw new createError.InternalServerError('Invalid user even though isAuthenticated() is true')
-    res.redirect('/login')
-  }
-
+router.get('/', (req, res) => {
   const helpHtml = `<div>Help Page</div>`
   res.send(page('Help', helpHtml, user));
 });
