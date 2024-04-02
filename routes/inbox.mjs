ca2faf426c15e8a38bc7549aa7b84a38a2988f61
Ray <raypeaslee@gmail.com>
2024-04-02 17:01:39 -0400

    save this much

--- routes/inbox.mjs
+++ routes/inbox.mjs
@@ -78,8 +78,17 @@ router.get('/', passport.authenticate('session'), wrap(async (req, res) => {
     feed += `
     <div class="card mb-3">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+        <h5 class="card-title">
+          <a href="${user.actorId}">
+            <img src="/img/ray.jpg" class="border rounded-circle me-2 img-fluid" alt="profile picture" style="height: 2.5rem">
+          </a>
+          <a href="${user.actorId}" class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover mb-0">
+            ${user.username}
+          </a>
+            <p class="text-muted d-inline">
+              10h ago
+            </p>
+        </h5>
         <p class="card-text">${ item.object.content }</p>
       </div>
       <img src="/img/greg-moore.jpg" class="card-img-bottom img-fluid" alt="Greg Moore">
